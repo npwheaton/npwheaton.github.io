b6ee6c9b2e12e3e171754a6304b0e0891e4ae881
Nate Wheaton <57465272+npwheaton@users.noreply.github.com>
2025-05-03 22:40:56 -0500

    Update graphicsTest.js

--- graphicsTest.js
+++ graphicsTest.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.174.0/three.core.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@<version>/build/three.module.js';
 
 //global variables for matrix
 const canvas = document.getElementById('canvas1');
